09faba124eb0add7e8f2919aa358324581788bbc
Eyal Betzalel <eyalbatzul@gmail.com>
2020-12-07 20:09:36 +0200

    Update gmpm.py

--- dataset/gmpm.py
+++ dataset/gmpm.py
@@ -45,7 +45,7 @@ def dataset(batch_size=32, image_size=32, buffer_size=10000):
         return train, test
     
     def clusters_to_images(samples, pathToCluster): 
-        
+        import ipdb; ipdb.set_trace()
         clusters = np.load(pathToCluster)
         samples = [np.reshape(np.rint(127.5 * (clusters[s] + 1.0)), [32, 32, 3]).astype(np.uint8) for s in samples]
         
