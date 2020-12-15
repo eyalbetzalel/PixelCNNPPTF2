42f6cf66c23e53d7a5e5fbc2230bc3294657007b
eyalbe <noreply@example.com>
2020-12-15 20:46:58 +0200

    test bla

--- train.py
+++ train.py
@@ -116,8 +116,8 @@ def train(
                     # loss = logistic_mixture_loss(
                     #     inputs, mixture, num_mixtures=model.num_mixtures
                     # )
-                    # import ipdb;ipdb.set_trace()
 
+                    import ipdb;ipdb.set_trace()
                     scce = tf.keras.losses.SparseCategoricalCrossentropy()
                     loss = scce(inputs, mixture).numpy()
 
