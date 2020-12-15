fce394fa2014cdef9a5f8c7e244ba603ef753383
eyalbe <noreply@example.com>
2020-12-15 20:15:38 +0200

    test bla

--- train.py
+++ train.py
@@ -91,6 +91,7 @@ def train(
         def clusters_to_images(samples):
             test = tf.gather(clusters,samples)
 
+
             # samples = samples.numpy()
             # samples = np.reshape(np.rint(127.5 * (clusters[samples.astype(int).tolist()] + 1.0)), [32, 32, 3])
             # samples = tf.convert_to_tensor(samples, np.float32)
